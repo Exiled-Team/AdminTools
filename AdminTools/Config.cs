a956d9ab706e5206ef637f7b3f6d6c75138aa78b
Thunder <thunder@scpetgame.com>
2022-08-06 19:22:29 -0400

    Doc consistency pt.2

--- AdminTools/Config.cs
+++ AdminTools/Config.cs
@@ -10,7 +10,7 @@ namespace AdminTools
         public bool IsEnabled { get; set; } = true;
         [Description("Should the tutorial class be in God Mode? Default: true")]
         public bool GodTuts { get; set; } = true;
-        [Description("Unjail all jailed players automatically when the round restarts?")]
+        [Description("Unjail all jailed players automatically when the round restarts? Default: false")]
         public bool ClearJailsOnRestart { get; set; } = false;
     }
 }
