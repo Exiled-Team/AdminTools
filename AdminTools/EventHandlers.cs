8e9e973bbaa755e1d3a10cfe34f7ae0f02849474
AtomSnow <51972322+AtomSnow@users.noreply.github.com>
2023-02-17 12:46:13 +0100

    Fix Tutorial position due to update

--- AdminTools/EventHandlers.cs
+++ AdminTools/EventHandlers.cs
@@ -186,7 +186,7 @@ namespace AdminTools
 			yield return Timing.WaitForSeconds(1f);
 			player.ClearInventory(false);
 			player.Role.Set(RoleTypeId.Tutorial, SpawnReason.ForceClass, RoleSpawnFlags.None);
-			player.Position = new Vector3(53f, 1020f, -44f);
+			
 		}
 
 		public static IEnumerator<float> DoUnJail(Player player)
